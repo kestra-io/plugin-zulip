0c1fcec07e386e721852662695916f406cc5d57a
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2025-12-25 13:04:07 +0530

    feat(): introduce Zulip plugin (#4)

---
+++ src/main/java/io/kestra/plugin/zulip/ZulipExecution.java
@@ -0,0 +1,71 @@
+package io.kestra.plugin.zulip;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.plugins.notifications.ExecutionInterface;
+import io.kestra.core.plugins.notifications.ExecutionService;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Map;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Send a Zulip message with the execution information.",
+    description = "The message will include a link to the execution page in the UI along with the execution ID, namespace, flow name, the start date, duration, and the final status of the execution. If failed, then the task that led to the failure is specified.\n\n" +
+    "Use this notification task only in a flow that has a [Flow trigger](https://kestra.io/docs/administrator-guide/monitoring#alerting). Don't use this notification task in `errors` tasks. Instead, for `errors` tasks, use the [ZulipIncomingWebhook](https://kestra.io/plugins/plugin-zulip/io.kestra.plugin.zulip.zulipincomingwebhook) task."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send a Zulip notification on a failed flow execution.",
+            full = true,
+            code = """
+                id: failure_alert
+                namespace: company.team
+
+                tasks:
+                  - id: send_alert
+                    type: io.kestra.plugin.zulip.ZulipExecution
+                    url: "{{ secret('ZULIP_WEBHOOK') }}" # format: https://yourZulipDomain.zulipchat.com/api/v1/external/INTEGRATION_NAME?api_key=API_KEY
+                    channel: "#general"
+                    executionId: "{{trigger.executionId}}"
+
+                triggers:
+                  - id: failed_prod_workflows
+                    type: io.kestra.plugin.core.trigger.Flow
+                    conditions:
+                      - type: io.kestra.plugin.core.condition.ExecutionStatus
+                        in:
+                          - FAILED
+                          - WARNING
+                      - type: io.kestra.plugin.core.condition.ExecutionNamespace
+                        namespace: prod
+                        prefix: true
+                """
+        )
+    },
+    aliases = "io.kestra.plugin.notifications.zulip.ZulipExecution"
+)
+public class ZulipExecution extends ZulipTemplate implements ExecutionInterface {
+    @Builder.Default
+    private final Property<String> executionId = Property.ofExpression("{{ execution.id }}");
+    private Property<Map<String, Object>> customFields;
+    private Property<String> customMessage;
+
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        this.templateUri = Property.ofValue("zulip-template.peb");
+        this.templateRenderMap = Property.ofValue(ExecutionService.executionMap(runContext, this));
+
+        return super.run(runContext);
+    }
+}
---
+++ src/main/java/io/kestra/plugin/zulip/ZulipIncomingWebhook.java
@@ -0,0 +1,145 @@
+package io.kestra.plugin.zulip;
+
+import io.kestra.core.http.HttpRequest;
+import io.kestra.core.http.HttpResponse;
+import io.kestra.core.http.client.HttpClient;
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.VoidOutput;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.net.URI;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Send a Zulip message using an Incoming Webhook.",
+    description = "Add this task to send direct Zulip notifications. Check the <a href=\"https://api.zulip.com/messaging/webhooks\">Zulip documentation</a> for more details."
+)
+@Plugin(
+    examples = {
+        @Example(
+            title = "Send a Zulip notification on a failed flow execution.",
+            full = true,
+            code = """
+                id: unreliable_flow
+                namespace: company.team
+
+                tasks:
+                  - id: fail
+                    type: io.kestra.plugin.scripts.shell.Commands
+                    runner: PROCESS
+                    commands:
+                      - exit 1
+
+                errors:
+                  - id: alert_on_failure
+                    type: io.kestra.plugin.zulip.ZulipIncomingWebhook
+                    url: "{{ secret('ZULIP_WEBHOOK') }}" # https://yourZulipDomain.zulipchat.com/api/v1/external/INTEGRATION_NAME?api_key=API_KEY
+                    payload: |
+                      {
+                        "text": "Failure alert for flow {{ flow.namespace }}.{{ flow.id }} with ID {{ execution.id }}"
+                      }
+                """
+        ),
+        @Example(
+            title = "Send a Zulip message via incoming webhook with a text argument.",
+            full = true,
+            code = """
+                id: zulip_incoming_webhook
+                namespace: company.team
+
+                tasks:
+                  - id: send_zulip_message
+                    type: io.kestra.plugin.zulip.ZulipIncomingWebhook
+                    url: "{{ secret('ZULIP_WEBHOOK') }}" # https://yourZulipDomain.zulipchat.com/api/v1/external/INTEGRATION_NAME?api_key=API_KEY
+                    payload: |
+                      {
+                        "text": "Hello from the workflow {{ flow.id }}"
+                      }
+                """
+        ),
+        @Example(
+            title = "Send a Zulip message via incoming webhook with a blocks argument, read more on blocks <a href=\"https://api.zulip.com/reference/block-kit/blocks\">here</a>.",
+            full = true,
+            code = """
+                id: zulip_incoming_webhook
+                namespace: company.team
+
+                tasks:
+                  - id: send_zulip_message
+                    type: io.kestra.plugin.zulip.ZulipIncomingWebhook
+                    url: "{{ secret('ZULIP_WEBHOOK') }}" # format: https://yourZulipDomain.zulipchat.com/api/v1/external/INTEGRATION_NAME?api_key=API_KEY
+                    payload: |
+                      {
+                        "blocks": [
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "Hello from the workflow *{{ flow.id }}*"
+                                }
+                            }
+                        ]
+                      }
+                """
+        ),
+    },
+    aliases = "io.kestra.plugin.notifications.zulip.ZulipIncomingWebhook"
+)
+public class ZulipIncomingWebhook extends AbstractZulipConnection {
+    @Schema(
+        title = "Zulip incoming webhook URL",
+        description = "Check the <a href=\"https://zulip.com/api/incoming-webhooks-overview\">Incoming Webhook Integrations</a> documentation for more details."
+    )
+    @PluginProperty(dynamic = true)
+    @NotEmpty
+    private String url;
+
+    @Schema(
+        title = "Zulip message payload"
+    )
+    protected Property<String> payload;
+
+    @Override
+    public VoidOutput run(RunContext runContext) throws Exception {
+        String url = runContext.render(this.url);
+
+        try (HttpClient client = new HttpClient(runContext, super.httpClientConfigurationWithOptions())) {
+            String payload = runContext.render(this.payload).as(String.class).orElse(null);
+
+            runContext.logger().debug("Send Zulip webhook: {}", payload);
+            HttpRequest.HttpRequestBuilder requestBuilder = createRequestBuilder(runContext)
+                .addHeader("Content-Type", "application/json")
+                .uri(URI.create(url))
+                .method("POST")
+                .body(HttpRequest.StringRequestBody.builder()
+                    .content(payload)
+                    .build());
+
+            HttpRequest request = requestBuilder.build();
+
+            HttpResponse<String> response = client.request(request, String.class);
+
+            runContext.logger().debug("Response: {}", response.getBody());
+
+            if (response.getStatus().getCode() == 200) {
+                runContext.logger().info("Request succeeded");
+            }
+        }
+
+        return null;
+    }
+}
